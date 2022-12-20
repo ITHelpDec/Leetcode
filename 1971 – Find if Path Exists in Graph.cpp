ec3daaa14356d17543a9b9160b036f394fad76fe
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-20 13:39:57 +0000

    Update 1971 – Find if Path Exists in Graph.cpp

--- 1971 – Find if Path Exists in Graph.cpp
+++ 1971 – Find if Path Exists in Graph.cpp
@@ -10,8 +10,7 @@
 
 class Solution {
 public:
-    bool validPath(int n, std::vector<std::vector<int>>& edges,
-                   int source, int destination) {
+    bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
         
         bool v[n];
         std::memset(v, false, sizeof(v));
@@ -47,7 +46,7 @@ public:
 /*   Solution using Union-Find   */
 // // // // // // // // // // // //
 
-// 1: create vector to represent each node
+// 1: create vector to represent each node (and / or rank)
 // 2: work out the parent of each node
 // 3: check that source and destination have the same parent ("are connected")
 
@@ -61,28 +60,35 @@ public:
     }
     
 private:
-    std::vector<int> parents;
+    std::vector<int> parents, rank;
     
     int find(const int&);
     
     void createParentVec(const int&);
+    
     void setParent(const int&, const int&);
+    void setParent2(const int&, const int&);
+    
     void setParents(const std::vector<std::vector<int>>&);
     
     void printParentVec(const std::vector<int>&);
 };
 
+// path compression find()
 inline int Solution2::find(const int &x)
 {
-    return parents[x] == x ? x : find(parents[x]);
+    // return parents[x] == x ? x : find(parents[x]);
+    return parents[x] == x ? parents[x] : parents[x] = find(parents[x]);
 }
 
 inline void Solution2::createParentVec(const int &n)
 {
+    rank.resize(n);
     parents.resize(n);
     std::iota(parents.begin(), parents.end(), 0);
 }
 
+// naive linking
 inline void Solution2::setParent(const int &x, const int &y)
 {
     if (x != y) {
@@ -91,11 +97,24 @@ inline void Solution2::setParent(const int &x, const int &y)
     }
 }
 
+// linking by rank
+inline void Solution2::setParent2(const int &x, const int &y)
+{
+    int a = find(x), b = find(y);
+    
+    if (a != b) {
+        if (rank[a] < rank[b]) { std::swap(a, b); }
+        parents[b] = a;
+        if (rank[a] == rank[b]) { ++rank[a]; }
+    }
+}
+
 inline void Solution2::setParents(const std::vector<std::vector<int>> &edges)
 {
     for (const auto &edge : edges)
         if ( find(edge.front()) != find(edge.back()) )
             setParent(edge.front(), edge.back());
+            // setParent2(edge.front(), edge.back());
 }
 
 inline void Solution2::printParentVec(const std::vector<int> &parents) {
@@ -114,32 +133,36 @@ void printTest(const std::vector<std::vector<int>> &test) {
     } std::cout << std::endl;
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 int main()
 {
     std::vector<std::vector<int>> test1 = { {0,1},{1,2},{2,0} };
     std::vector<std::vector<int>> test2 = { {0,1},{0,2},{3,5},{5,4},{4,3} };
     
-    std::vector<std::vector<int>> test3(100, std::vector<int>(2, 0));
-    for (auto &v : test3) { v.front() = rand() % 10; v.back() = rand() % 10; }
+     std::vector<std::vector<int>> test3(100, std::vector<int>(2, 0));
+     for (auto &v : test3) { v.front() = rand() % 10; v.back() = rand() % 10; }
     
     // printTest(test3);
-    // test3 = {
-    // { 7, 9 }, { 3, 8 }, { 0, 2 }, { 4, 8 }, { 3, 9 }, { 0, 5 }, { 2, 2 },
-    // { 7, 3 }, { 7, 9 }, { 0, 2 }, { 3, 9 }, { 9, 7 }, { 0, 3 }, { 9, 8 },
-    // { 6, 5 }, { 7, 6 }, { 2, 7 }, { 0, 3 }, { 9, 9 }, { 9, 1 }, { 7, 2 },
-    // { 3, 6 }, { 5, 5 }, { 8, 1 }, { 4, 7 }, { 1, 3 }, { 8, 4 }, { 8, 0 },
-    // { 4, 6 }, { 0, 3 }, { 2, 6 }, { 9, 4 }, { 1, 3 }, { 7, 8 }, { 8, 3 },
-    // { 8, 1 }, { 5, 3 }, { 5, 4 }, { 3, 6 }, { 5, 9 }, { 5, 4 }, { 9, 1 },
-    // { 7, 5 }, { 5, 4 }, { 1, 8 }, { 8, 3 }, { 5, 2 }, { 2, 6 }, { 6, 7 },
-    // { 8, 4 }, { 1, 7 }, { 1, 8 }, { 7, 8 }, { 7, 7 }, { 4, 4 }, { 9, 1 },
-    // { 5, 5 }, { 5, 8 }, { 2, 9 }, { 8, 2 }, { 0, 7 }, { 4, 8 }, { 5, 8 },
-    // { 3, 0 }, { 6, 2 }, { 2, 5 }, { 2, 2 }, { 7, 1 }, { 5, 2 }, { 1, 1 },
-    // { 0, 1 }, { 8, 7 }, { 6, 0 }, { 0, 9 }, { 5, 6 }, { 2, 8 }, { 5, 9 },
-    // { 4, 3 }, { 9, 0 }, { 5, 6 }, { 3, 6 }, { 3, 0 }, { 8, 4 }, { 0, 4 },
-    // { 6, 4 }, { 6, 4 }, { 3, 8 }, { 4, 0 }, { 0, 9 }, { 3, 7 }, { 5, 7 },
-    // { 2, 9 }, { 7, 5 }, { 6, 5 }, { 3, 0 }, { 4, 8 }, { 5, 5 }, { 5, 4 },
-    // { 2, 1 }, { 9, 2 }
-    // };
+    
+    // example random test case
+//     std::vector<std::vector<int>> test3 = {
+//     { 7, 9 }, { 3, 8 }, { 0, 2 }, { 4, 8 }, { 3, 9 }, { 0, 5 }, { 2, 2 },
+//     { 7, 3 }, { 7, 9 }, { 0, 2 }, { 3, 9 }, { 9, 7 }, { 0, 3 }, { 9, 8 },
+//     { 6, 5 }, { 7, 6 }, { 2, 7 }, { 0, 3 }, { 9, 9 }, { 9, 1 }, { 7, 2 },
+//     { 3, 6 }, { 5, 5 }, { 8, 1 }, { 4, 7 }, { 1, 3 }, { 8, 4 }, { 8, 0 },
+//     { 4, 6 }, { 0, 3 }, { 2, 6 }, { 9, 4 }, { 1, 3 }, { 7, 8 }, { 8, 3 },
+//     { 8, 1 }, { 5, 3 }, { 5, 4 }, { 3, 6 }, { 5, 9 }, { 5, 4 }, { 9, 1 },
+//     { 7, 5 }, { 5, 4 }, { 1, 8 }, { 8, 3 }, { 5, 2 }, { 2, 6 }, { 6, 7 },
+//     { 8, 4 }, { 1, 7 }, { 1, 8 }, { 7, 8 }, { 7, 7 }, { 4, 4 }, { 9, 1 },
+//     { 5, 5 }, { 5, 8 }, { 2, 9 }, { 8, 2 }, { 0, 7 }, { 4, 8 }, { 5, 8 },
+//     { 3, 0 }, { 6, 2 }, { 2, 5 }, { 2, 2 }, { 7, 1 }, { 5, 2 }, { 1, 1 },
+//     { 0, 1 }, { 8, 7 }, { 6, 0 }, { 0, 9 }, { 5, 6 }, { 2, 8 }, { 5, 9 },
+//     { 4, 3 }, { 9, 0 }, { 5, 6 }, { 3, 6 }, { 3, 0 }, { 8, 4 }, { 0, 4 },
+//     { 6, 4 }, { 6, 4 }, { 3, 8 }, { 4, 0 }, { 0, 9 }, { 3, 7 }, { 5, 7 },
+//     { 2, 9 }, { 7, 5 }, { 6, 5 }, { 3, 0 }, { 4, 8 }, { 5, 5 }, { 5, 4 },
+//     { 2, 1 }, { 9, 2 }
+//     };
     
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
