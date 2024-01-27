36fff1d31407f15fce90c9e6b4582a96b5c68eb4
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-01-27 18:10:22 +0000

    change previous $`O(n)`$ solution
      * I don't believe my previous solution was $`O(n)`$ was accurate

--- 0001 - Two Sum/main.cpp
+++ 0001 - Two Sum/main.cpp
@@ -15,12 +15,16 @@ class Solution2 {
 public:
     std::vector<int> twoSum(const std::vector<int> &nums, int target)
     {
-        std::unordered_multiset umset(nums.begin(),nums.end());
+        // we aim to split the elements in "nums" into their corresponding values and positions
+        std::unordered_map<int, int> uimap;
 
-        for (int i = 0; i != nums.size() - 1; ++i)
-            if (umset.count(target - nums[i]))
-                for (int j = i + 1; j != nums.size(); ++j)
-                    if (nums[i] + nums[j] == target) { return { i, j }; }
+        // add value and position as we go along to both save on space and only refer backwards from our current position in nums
+        for (int i = 0; i != nums.size(); ++i) {
+            if (uimap.count(target - nums[i]))
+                return { uimap[target - nums[i]], i };
+            uimap[nums[i]] = i;
+        }
+        
         return {};
     }
 };
