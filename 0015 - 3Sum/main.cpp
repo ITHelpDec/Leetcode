30bbc4d142093f0e7a55a63e173ee660528a43b9
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-07 01:08:26 +0000

    add two-pointer approach

--- 0015 - 3Sum/main.cpp
+++ 0015 - 3Sum/main.cpp
@@ -18,3 +18,41 @@ public:
         return std::vector(result.begin(), result.end());
     }
 };
+
+// better time complexity
+// O(nlog(n)) due to sort, then two pointer approach to narrow in on suitable matches
+class Solution2 {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
+        std::vector<std::vector<int>> result;
+        if (nums.empty()) { return result; }
+
+        std::sort(nums.begin(), nums.end());
+
+        std::size_t n = nums.size();
+        for (int i = 0; i != n; ++i) {
+            int low  = i + 1;
+            int high = n - 1;
+
+            while (low < high) {
+                if (nums[i] + nums[low] + nums[high] < 0) {
+                    ++low;
+                } else if (nums[i] + nums[low] + nums[high] > 0) {
+                    --high;
+                } else {
+                    result.push_back({ nums[i], nums[low], nums[high] });
+
+                    int temp_index1 = low;
+                    int temp_index2 = high;
+
+                    while (low < high && nums[low]  == nums[temp_index1]) { ++low;  }
+                    while (low < high && nums[high] == nums[temp_index2]) { --high; }
+                }
+            }
+
+            while (i + 1 < n && nums[i] == nums[i + 1]) { ++i; }
+        }
+
+        return result;
+    }
+};
