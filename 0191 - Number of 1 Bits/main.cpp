2544d0dcc0724419d135b99c07f7f245819d897a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-08 23:41:02 +0000

    add more bitwise solutions

--- 0191 - Number of 1 Bits/main.cpp
+++ 0191 - Number of 1 Bits/main.cpp
@@ -13,3 +13,33 @@ public:
         return count;
     }
 };
+
+// Not sure how I would classify this solution - linear(?), given that we're iterating through hte bits?
+class Solution2 {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+
+        return count;
+    }
+};
+
+// we can also take advantage of member functions from std::bitset<>
+class Solution3 {
+public:
+    int hammingWeight(uint32_t n) {
+        return std::bitset<32>(n).count();
+    }
+};
+
+class Solution4 {
+public:
+    int hammingWeight(uint32_t n) {
+        return std::popcount(n);
+    }
+};
