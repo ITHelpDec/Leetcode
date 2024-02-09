265a2e4eaad86a21d12057fc4c23245a5d419d1d
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-09 23:29:06 +0000

    add divide and conquer solution

--- 0190 - Reverse Bits/main.cpp
+++ 0190 - Reverse Bits/main.cpp
@@ -16,3 +16,17 @@ public:
         return bs.to_ulong();
     }
 };
+
+// wild divide and conquer solution (pain to write out)
+// premise is shift left half to other side, then vice versa versa - keep dividing until we up with alternating bits
+class Solution2 {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n & 0b11111111111111110000000000000000) >> 16 | (n & 0b00000000000000001111111111111111) << 16;
+        n = (n & 0b11111111000000001111111100000000) >>  8 | (n & 0b00000000111111110000000011111111) <<  8;
+        n = (n & 0b11110000111100001111000011110000) >>  4 | (n & 0b00001111000011110000111100001111) <<  4;
+        n = (n & 0b11001100110011001100110011001100) >>  2 | (n & 0b00110011001100110011001100110011) <<  2;
+        n = (n & 0b10101010101010101010101010101010) >>  1 | (n & 0b01010101010101010101010101010101) <<  1;
+        return n;
+    }
+};
