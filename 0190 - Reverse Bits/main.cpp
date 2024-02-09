83d6bf0f2029c2cc1f3f5aa608fb27e5437b37bf
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-09 23:46:11 +0000

    add two solutions
      * divide and conquer
      * solution from stanford

--- 0190 - Reverse Bits/main.cpp
+++ 0190 - Reverse Bits/main.cpp
@@ -30,3 +30,23 @@ public:
         return n;
     }
 };
+
+// soltuion provided by standfor page
+// http://graphics.stanford.edu/~seander/bithacks.html#BitReverseTable
+class Solution3 {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t r = n;
+        int      s = sizeof(n) * CHAR_BIT - 1;
+
+        for (n >>= 1; n; n >>= 1) {   
+            r <<= 1;
+            r |= n & 1;
+            --s;
+        }
+        
+        r <<= s;
+
+        return r;
+    }
+};
