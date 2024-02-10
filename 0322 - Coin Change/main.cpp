fb0adf359fff35251df9b3976dc91a8c72950134
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-10 16:26:23 +0000

    add cleaner dp solution

--- 0322 - Coin Change/main.cpp
+++ 0322 - Coin Change/main.cpp
@@ -8,7 +8,7 @@ public:
 
         std::sort(coins.begin(), coins.end());
 
-        for (int i = 0; i != amount; ++i) {
+        for (int i = 1; i != amount; ++i) {
             for (const int coin : coins) {
                 if (i - coin < 0) { break; }
                 if (dp[i - coin] != INT_MAX) {
@@ -16,6 +16,27 @@ public:
                 }
             }
         }
+        
         return dp[--amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+
+// slightly cleaner solution, inverting the loops and opting for std::size_t in place of int
+// doesn't appear to work with std::numeric_limits<std::size_t>::max() in place of INT_MAX, though...
+class Solution2 {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        std::vector<std::size_t> dp(amount + 1, INT_MAX); 
+        dp[0] = 0;
+
+        std::sort(coins.begin(), coins.end()); 
+        
+        for (const std::size_t coin : coins) {
+            for (std::size_t i = coin; i <= amount; ++i){
+                dp[i] = std::min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
