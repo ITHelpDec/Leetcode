2fa61378f371451c40baeea655558b439a214d66
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-10 14:35:03 +0000

    add bottom up solution

--- 0070 - Climbing Stairs/main.cpp
+++ 0070 - Climbing Stairs/main.cpp
@@ -20,3 +20,22 @@ public:
         return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
+
+// bottom-up solutiuo
+class Solution3 {
+public:
+    int climbStairs(int n) {
+        if (n < 4) { return n; }
+        
+        int a = 2;
+        int b = 3;
+
+        for (int i = 4; i != n; ++i) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return a + b;
+    }
+};
