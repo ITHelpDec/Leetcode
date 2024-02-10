b63d28274652e252581a92b2632b0f4b9d6d877b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-02-10 14:20:58 +0000

    add memoisation solution

--- 0070 - Climbing Stairs/main.cpp
+++ 0070 - Climbing Stairs/main.cpp
@@ -7,3 +7,16 @@ public:
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
+
+// memoisation solution
+// avoid unnecessary recusrive calculations by storing them for future referecne
+std::vector<int> memo(46);
+
+class Solution2 {
+public:
+    int climbStairs(int n) {
+        if (n < 4)   { return memo[n] = n; }
+        if (memo[n]) { return memo[n]; }
+        return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
